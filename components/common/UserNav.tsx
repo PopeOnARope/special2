fcfbf527efa1c1c0e7fa31f491b034d23a69b776
Nats <natsdevstudio@gmail.com>
2021-08-14 01:22:02 +0400

    Added custom slider

--- components/common/UserNav.tsx
+++ components/common/UserNav.tsx
@@ -10,12 +10,14 @@ interface Props {
   isScrollingInPage: boolean
 }
 
-const UserNav: FC<Props> = ({isScrollingInPage}) => {
+const UserNav: FC<Props> = ({ isScrollingInPage }) => {
   const cart = useCart()
-  const totalItems = cart?.lineItems?.map(item=>item.quantity).reduce((num, tot)=> num + tot)
+  const totalItems = cart?.lineItems
+    ?.map((item) => item.quantity)
+    .reduce((num, tot) => num + tot)
 
   const { toggleCart, toggleSideNav, navPrimaryColor, displaySideNav } = useUI()
-  const PC = isScrollingInPage ?   'black' : navPrimaryColor
+  const PC = isScrollingInPage ? 'black' : navPrimaryColor
   const navItemStyles = {
     background: 'none',
     border: PC,
@@ -24,10 +26,16 @@ const UserNav: FC<Props> = ({isScrollingInPage}) => {
     ' svg': { transition: 'all 0.25s', fill: PC },
   }
   return (
-    <Themed.div sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
-      <Button sx={{ ...navItemStyles, '&:focus': { outline: 0} }} onClick={toggleCart} aria-label="Cart">
+    <Themed.div
+      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}
+    >
+      <Button
+        sx={{ ...navItemStyles, '&:focus': { outline: 0 } }}
+        onClick={toggleCart}
+        aria-label="Cart"
+      >
         {/*<Bag/>*/}
-        <Themed.div sx={{  padding: '0px 15px'  }}>{totalItems}</Themed.div>
+        <Themed.div sx={{ padding: '0px 15px' }}>{totalItems}</Themed.div>
       </Button>
       {/*<Button sx={{ ...navItemStyles, '&:focus': { outline: 0} }} aria-label="Login">*/}
       {/*  <NounEyes sx={{ margin: 0 }} />*/}
---
+++ components/product/ProductBox.jsx
@@ -0,0 +1,366 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+//@ts-nocheck
+import React, { useMemo, useState, useEffect } from 'react'
+import { Themed, jsx } from 'theme-ui'
+import { Grid } from '@theme-ui/components'
+
+import { ArrowLeft, ChevronUp } from '../../components/icons'
+import OptionPicker from '@components/common/OptionPicker'
+import { NextSeo } from 'next-seo'
+import { useUI } from '@components/ui/context'
+import { useAddItemToCart } from '@lib/shopify/storefront-data-hooks'
+import {
+  prepareVariantsWithOptions,
+  prepareVariantsImages,
+  getPrice,
+} from '@lib/shopify/storefront-data-hooks/src/utils/product'
+import Image from 'next/image'
+import { LoadingDots, Sidebar } from '@components/ui'
+import ProductDetails from '@components/ProductDetails/ProductDetails'
+import Button from '../../blocks/Button/Button'
+
+const CustomSlider = ({ itemsCount = 1, index = 0 }) => {
+  return (
+    <div
+      sx={{
+        width: '100%',
+        height: '10px',
+        position: 'absolute',
+        top: 0,
+        zIndex: 50,
+      }}
+    >
+      {itemsCount > 1 ? (
+        <div
+          sx={{
+            backgroundColor: '#FFC391',
+            width: `${100 / itemsCount}%`,
+            height: '10px',
+            marginLeft: `${(100 / itemsCount) * index}%`,
+          }}
+        ></div>
+      ) : null}
+    </div>
+  )
+}
+
+const NextButton = ({ onClick }) => (
+  <button onClick={onClick} className="focus:outline-none">
+    <div
+      sx={{
+        color: 'white',
+        transform: 'rotate(90deg)',
+      }}
+      className="hover:pr-20"
+    >
+      <ChevronUp />
+    </div>
+  </button>
+)
+const PreviousButton = ({ onClick }) => (
+  <button onClick={onClick} className="focus:outline-none">
+    <div sx={{ color: 'white', transform: 'rotate(270deg)' }}>
+      <ChevronUp />
+    </div>
+  </button>
+)
+
+const ProductBox = ({
+  product,
+  renderSeo = true,
+  description = product.description,
+  title = product.title,
+}) => {
+  const addItem = useAddItemToCart()
+
+  const [loading, setLoading] = useState(false)
+  const [currentImage, setCurrentImage] = useState(0)
+  const numberOfImage = 5
+
+  const getNextImage = (num) => {
+    var nextIndex = currentImage + num
+
+    if (nextIndex < 0) {
+      nextIndex = numberOfImage
+    }
+
+    if (nextIndex >= numberOfImage) {
+      nextIndex = 0
+    }
+
+    console.log(nextIndex)
+
+    setCurrentImage(nextIndex)
+  }
+
+  const colors = product?.options
+    ?.find((option) => option?.name?.toLowerCase() === 'color')
+    ?.values?.map((op) => op.value)
+
+  const sizes = product?.options
+    ?.find((option) => option?.name?.toLowerCase() === 'size')
+    ?.values?.map((op) => op.value)
+
+  const variants = useMemo(
+    () => prepareVariantsWithOptions(product?.variants),
+    [product?.variants]
+  )
+
+  const images = useMemo(() => prepareVariantsImages(variants, 'color'), [
+    variants,
+  ])
+
+  const {
+    openCart,
+    toggleProductDetails,
+    displayProductDetails,
+    closeProductDetails,
+  } = useUI()
+
+  const [peakingImage, setPeakingImage] = useState(null)
+
+  const [variant, setVariant] = useState(variants[0] || {})
+  const [color, setColor] = useState(variant.color)
+  const [size, setSize] = useState(variant.size)
+
+  function IsJsonString(str) {
+    try {
+      JSON.parse(str)
+    } catch (e) {
+      return false
+    }
+    return true
+  }
+
+  useEffect(() => {
+    const newVariant = variants.find((variant) => {
+      return (
+        (variant.size === size || !size) && (variant.color === color || !color)
+      )
+    })
+
+    if (variant.id !== newVariant?.id) {
+      setVariant(newVariant)
+      setPeakingImage(null)
+    }
+  }, [size, color, variants, variant.id])
+
+  const addToCart = async () => {
+    setLoading(true)
+    try {
+      await addItem(variant.id, 1)
+      console.log('item added')
+      openCart()
+      setLoading(false)
+    } catch (err) {
+      console.log({ err })
+      setLoading(false)
+    }
+  }
+
+  console.log('product.images.length')
+  console.log(product.images.length)
+  console.log('***************')
+
+  console.log('variants')
+  console.log(variants.length)
+  console.log('***************')
+
+  if (variant.image) {
+    console.log('variant.image.src')
+    console.log(variant.image.src)
+    console.log('***************')
+  }
+
+  console.log('size')
+  console.log(size)
+  console.log('***************')
+
+  console.log('color')
+  console.log(color)
+  console.log('***************')
+
+  return (
+    <React.Fragment>
+      {renderSeo && (
+        <NextSeo
+          title={title}
+          description={description}
+          openGraph={{
+            type: 'website',
+            title: title,
+            description: description,
+            images: [
+              {
+                url: product.images?.[0]?.src,
+                width: 800,
+                height: 600,
+                alt: title,
+              },
+            ],
+          }}
+        />
+      )}
+
+      <div
+        sx={{
+          position: 'relative',
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: 'black',
+        }}
+        className="type-wrapper"
+      >
+        <CustomSlider itemsCount={numberOfImage} index={currentImage} />
+
+        <div
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            zIndex: '20',
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            padding: 32,
+          }}
+        >
+          <div className="flex flex-row relative justify-between items-start w-full h-1/2">
+            <PreviousButton
+              onClick={() => {
+                const img = peakingImage || variant.image
+                const peakingImageIndex = product.images.indexOf(img)
+                const newPeakingImageIndex =
+                  peakingImageIndex === 0
+                    ? product.images.length - 1
+                    : peakingImageIndex - 1
+                setPeakingImage(product.images[newPeakingImageIndex])
+                getNextImage(-1)
+              }}
+            />
+
+            <button onClick={() => getNextImage(1)}>Next</button>
+
+            <NextButton
+              onClick={() => {
+                const img = peakingImage || variant.image
+                const peakingImageIndex = product.images.indexOf(img)
+                const newPeakingImageIndex =
+                  peakingImageIndex === product.images.length - 1
+                    ? 0
+                    : peakingImageIndex + 1
+                setPeakingImage(product.images[newPeakingImageIndex])
+                getNextImage(1)
+              }}
+            />
+
+            <div
+              className="text-white hover:cursor-pointer"
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                marginLeft: '-6rem',
+                transform: 'rotate(90deg)',
+              }}
+            >
+              <button
+                style={{
+                  display: 'flex',
+                }}
+                onClick={toggleProductDetails}
+              >
+                Details and Specs <ArrowLeft orientation="down" marginTop="0" />
+              </button>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-3xl text-white mb-0 pb-0">{title}</h1>
+            <Grid columns={2}>
+              {colors?.length && (
+                <OptionPicker
+                  key="Color"
+                  name="Color"
+                  options={colors}
+                  selected={color}
+                  onChange={(event) => setColor(event.target.value)}
+                />
+              )}
+              {sizes?.length && (
+                <OptionPicker
+                  key="Size"
+                  name="Size"
+                  options={sizes}
+                  selected={size}
+                  onChange={(event) => setSize(event.target.value)}
+                />
+              )}
+            </Grid>
+            <Button
+              sx={{
+                background:
+                  'linear-gradient(to left, #000 50%, #FFC391 50%) right',
+                transition: '.5s ease-out',
+                backgroundSize: '200%',
+                ' &:hover': {
+                  boxShadow: '6px 5px 10px rgba(0,0,0,0.2)',
+                  color: '#000',
+                  backgroundPosition: 'left',
+                },
+              }}
+              name="add-to-cart"
+              disabled={loading}
+              onClick={addToCart}
+            >
+              <span className="flex flex-row justify-between mr-2">
+                <span>Bag {loading && <LoadingDots />}</span>
+                {getPrice(variant.priceV2.amount, variant.priceV2.currencyCode)}
+              </span>
+            </Button>
+          </div>
+        </div>
+        <div
+          sx={{
+            border: '1px solid gray',
+            padding: 2,
+            marginBottom: 2,
+            position: 'absolute',
+            zIndex: '0',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          {variant.image && (
+            <Image
+              src={peakingImage?.src || variant.image.src}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              alt={title}
+              priority
+              quality={100}
+              className="object-center object-cover"
+            />
+          )}
+        </div>
+      </div>
+      <Sidebar
+        open={displayProductDetails}
+        onClose={closeProductDetails}
+        from="left"
+      >
+        <ProductDetails
+          details={
+            IsJsonString(product.description)
+              ? JSON.parse(product.description)
+              : {}
+          }
+        />
+      </Sidebar>
+    </React.Fragment>
+  )
+}
+
+export default ProductBox
