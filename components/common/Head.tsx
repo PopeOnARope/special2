15217d80a65b5881356e890fc19a8fb8421aadfe
popeonarope <johnvepope@gmail.com>
2021-10-06 13:35:52 -0400

    replace url with proper url for OG

--- components/common/Head.tsx
+++ components/common/Head.tsx
@@ -5,7 +5,7 @@ import { DefaultSeo } from 'next-seo'
 const Head: FC<{ seoInfo: any }> = (props) => {
   return (
     <>
-      <DefaultSeo {...props.seoInfo} />
+      {/*<DefaultSeo {...props.seoInfo} />*/}
       <NextHead>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
@@ -14,6 +14,10 @@ const Head: FC<{ seoInfo: any }> = (props) => {
           type="image/png"
           href="https://cdn.builder.io/api/v1/image/assets%2Fd58e15993bf84115968f2dd035ee71a4%2F14580646b40e449aab23634dda0c7787"
         />
+        <meta property="og:title" content="Spec_ial" />
+        <meta property="og:description" content="Spec_ial upends traditional eyewear with provocative exploration. Crafted in micro-micro  — with ranging influences from art to astrology – our pieces cross over from accessory into sculpture. Conceptualized between New York, Los Angeles and Edinburgh, Spec_ial’s eyewear ignores boundaries, leaving you ... Spec_ial." />
+        <meta property="og:url" content="https://spec-ial.com" />
+        {/*<meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />*/}
       </NextHead>
     </>
   )
