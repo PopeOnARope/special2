1232a400108d6df83eeb6127a9d3edfc147efc61
popeonarope <johnvepope@gmail.com>
2022-01-10 17:19:11 -0500

    update fb sdk

--- blocks/ProductView/ProductView.tsx
+++ blocks/ProductView/ProductView.tsx
@@ -124,19 +124,26 @@ const ProductBox: React.FC<Props> = ({
     setIsMobile(isMobile())
     setHasRendered(true)
     console.log('track view content', {product})
-
-    fbEvent({
-      eventName: 'ViewContent',
-      products: [{
-        id: product.id,
-        quantity: 1,
-      }],
-      value: variant.price,
-      currency: 'USD',
-      enableStandardPixel: true,
-      emails: [Cookies.get('email')],
-      phones: [Cookies.get('phoneNumber')]
-    })
+    FB.api(
+      '/419048403222414/events',
+      'POST',
+      {"data":"[\n  {\n    \"event_name\": \"ViewContent\",\n    \"event_time\": 1641838139,\n    \"action_source\": \"email\",\n    \"user_data\": {\n      \"em\": [\n        \"7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068\"\n      ],\n      \"ph\": [\n        null\n      ]\n    },\n    \"custom_data\": {\n      \"currency\": \"USD\",\n      \"value\": \"142.52\"\n    }\n  }\n]","test_event_code":"TEST72881"},
+      function(response) {
+        console.log({response})
+      }
+    );
+  //   fbEvent({
+  //     eventName: 'ViewContent',
+  //     products: [{
+  //       id: product.id,
+  //       quantity: 1,
+  //     }],
+  //     value: variant.price,
+  //     currency: 'USD',
+  //     enableStandardPixel: false,
+  //     emails: [Cookies.get('email')],
+  //     phones: [Cookies.get('phoneNumber')]
+  //   })
   }, [])
 
   const peakingImageIndex = images
--- components/common/Pixel.js
+++ components/common/Pixel.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import facebookConfig from '@config/facebook'
+import env from '@config/env'
+import facebook from '@config/facebook'
 
+const Pixel = () =>(
+    <React.Fragment>
 
-export default () =>
-  <React.Fragment>
-    <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -12,10 +15,28 @@ export default () =>
       t.src=v;s=b.getElementsByTagName(e)[0];
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', ${facebookConfig.facebookPixelId});
-      fbq('track', 'PageView');` }}
-    />
-    <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=${facebookConfig.facebookPixelId}&ev=PageView&noscript=1" />` }}
-    />
-  </React.Fragment>
+      fbq('init', ${facebook.facebookPixelId});
+      fbq('track', 'PageView');`,
+        }}
+      />
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=${facebook.facebookPixelId}&ev=PageView&noscript=1" />`,
+        }}
+      />
+
+      <script
+        dangerouslySetInnerHTML={{__html: `window.fbAsyncInit = function() {
+        FB.init({
+          appId: '483880319830378',
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: 'v12.0'
+        });
+      }`}}/>
+      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"/>
+    </React.Fragment>
+  )
+
+export default Pixel
